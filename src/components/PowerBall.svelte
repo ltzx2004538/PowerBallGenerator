3e27a0cf38a8d87b40e2bd0802cc652e8f528d23
YurunYu <yurun.yu@martinit.com.au>
2024-02-04 12:49:21 +1100

    setup

---
+++ src/components/PowerBall.svelte
@@ -0,0 +1,16 @@
+<div class="powerball">
+	PowerBall
+</div>
+
+<style lang="scss">
+  .powerball {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: black;
+	padding: 1rem;
+	height: 50px;
+	width: 50px;
+	border: 1px solid black
+  }
+</style>
